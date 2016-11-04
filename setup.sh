204123adb52e31535dc8e6f36741fa126769359f
Sabeeh Khan <sabeeh.khan14@gmail.com>
2016-11-04 18:29:56 +0000

    setup, dto rm

--- setup.sh
+++ setup.sh
@@ -2,9 +2,6 @@
 # It is based on the following website: 
 # http://www.embedded-things.com/bbb/enable-canbus-on-the-beaglebone-black/
 #!/bin/bash
-dtc -O dtb -o BB-DCAN1-00A0.dtbo -b 0 -@ BB-DCAN1-00A0.dts
-sudo cp BB-DCAN1-00A0.dtbo /lib/firmware
-echo BB-DCAN1 > /sys/devices/platform/bone_capemgr/slots
 modprobe can
 modprobe can-dev
 modprobe can-raw
@@ -15,5 +12,5 @@ config-pin -a P8_13 low
 config-pin -a P9_26 can
 config-pin -a P9_24 can
 
-ip link set can1 up type can bitrate 1000000
+ip link set can1 up type can bitrate $1
 ifconfig can1 up
