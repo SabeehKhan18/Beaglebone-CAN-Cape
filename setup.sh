54b50f6b8c263dde2cb02a6dd85f1281b537849a
Sabeeh Khan <sabeeh.khan14@gmail.com>
2016-11-09 06:23:49 +0000

    setup installfiles

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
-# This script is for setting up the device tree overlay 
+# This script is for setting up the bone's gpio. 
 # It is based on the following website: 
 # http://www.embedded-things.com/bbb/enable-canbus-on-the-beaglebone-black/
+# The device tree was not used as the CAN bus was already enabled via config-pin.
 #!/bin/bash
+
 modprobe can
 modprobe can-dev
 modprobe can-raw
@@ -12,5 +14,12 @@ config-pin -a P8_13 low
 config-pin -a P9_26 can
 config-pin -a P9_24 can
 
-ip link set can1 up type can bitrate $1
+if [[ $# -eq 0 ]] ; then
+	echo 'Baudrate not given, please give one:'
+    read baud
+	ip link set can1 up type can bitrate $baud
+else
+	ip link set can1 up type can bitrate $1
+fi
+
 ifconfig can1 up
